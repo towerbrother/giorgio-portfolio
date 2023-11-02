ee6e26b0d72b6052c08654e238239655ccf2daf0
towerbrother <giorgio.torre8@gmail.com>
2023-11-02 21:09:10 +0100

    created some base components

--- app/components/base/MenuIcon.tsx
+++ app/components/base/MenuIcon.tsx
@@ -1,7 +1,7 @@
 import { FaBars, FaTimes } from 'react-icons/fa';
 
-import Button from '~/components/reusable/Button';
-import ConditionalWrapper from '~/components/reusable/ConditionalWrapper';
+import Button from '~/components/base/Button';
+import ConditionalWrapper from '~/components/base/ConditionalWrapper';
 
 type MenuIconProps = {
   iconType: 'bars' | 'cross';
@@ -19,12 +19,12 @@ const MenuIcon = ({
       position === 'relative' ? 'top-1' : 'top-8 right-8'
     } ml-2 xl:hidden`}
   >
-    <Button className="relative z-30" onClick={toggle}>
+    <Button className='relative z-30' onClick={toggle}>
       <ConditionalWrapper condition={iconType === 'bars'}>
-        <FaBars className="text-3xl text-neutral-800 hover:opacity-80 lg:text-4xl" />
+        <FaBars className='text-3xl text-neutral-800 hover:opacity-80 lg:text-4xl' />
       </ConditionalWrapper>
       <ConditionalWrapper condition={iconType === 'cross'}>
-        <FaTimes className="text-neutral-300 text-4xl" />
+        <FaTimes className='text-neutral-300 text-4xl' />
       </ConditionalWrapper>
     </Button>
   </div>
